b750de5dd6dbdcba3ea00f0b13dcb520c0c4a6de
Derk Kooi <d.p.kooi@vu.nl>
2020-06-26 14:01:28 +0200

    Move from HORTON 2 to HORTON 3.

--- fdm/pyscf_horton.py
+++ fdm/pyscf_horton.py
@@ -17,12 +17,18 @@
 
 import numpy as np
 
-from gbasis import get_gobasis, GOBasisFamily, GOBasis
-from iodata import IOData
+from gbasis.wrappers import from_pyscf
+from gbasis.integrals import overlap, moment
 
-import tempfile
+from pyscf import scf, ao2mo, fci, cc, mp
 
-from pyscf import scf, ao2mo, fci, cc, mp, tools
+
+def write_string(filename, string):
+    file = open(filename, 'w+')
+    file.write(string)
+    file.close()
+    return
+
 
 def RHF(mol_pyscf):
     '''Run a R(O)HF calculation on mol_pyscf, does a few sanity checks between
@@ -38,11 +44,11 @@ def RHF(mol_pyscf):
     '''
 
     # Calculate the overlaps and dipoles using pyscf
-    overlap = mol_pyscf.intor('int1e_ovlp')
+    overlap_pyscf = mol_pyscf.intor('int1e_ovlp')
     dipole = mol_pyscf.intor('int1e_r')
 
     # Run a RHF calculation on the molecule/atom
-    mf = scf.RHF(mol_pyscf).run() # Run RHF
+    mf = scf.RHF(mol_pyscf).run()  # Run RHF
 
     if mol_pyscf.spin == 0:
         # Make HF 1-RDM in AO basis
@@ -58,12 +64,12 @@ def RHF(mol_pyscf):
         mo_coeff_pyscf = mf.mo_coeff
 
         # Make HF 1-RDM in MO basis
-        rdm1_MO = np.zeros((nbasis,nbasis)) 
+        rdm1_MO = np.zeros((nbasis, nbasis))
         for i in range(nocc):
             rdm1_MO[i, i] = 2.
 
         # Check if the MO and AO basis RDM's are equal
-        assert np.allclose(mo_coeff_pyscf.dot(rdm1_MO.dot(mo_coeff_pyscf.T)),rdm1_HF) 
+        assert np.allclose(mo_coeff_pyscf.dot(rdm1_MO.dot(mo_coeff_pyscf.T)), rdm1_HF) 
 
     else:
         # Make HF 1-RDM in AO basis
@@ -77,56 +83,116 @@ def RHF(mol_pyscf):
 
         # Make HF 1-RDM in MO basis
         rdm1_MO = []
-        rdm1_MO.append(np.zeros((nbasis,nbasis)))
-        rdm1_MO.append(np.zeros((nbasis,nbasis)))
+        rdm1_MO.append(np.zeros((nbasis, nbasis)))
+        rdm1_MO.append(np.zeros((nbasis, nbasis)))
         for i in range(mol_pyscf.nelec[0]):
             rdm1_MO[0][i, i] = 1
         for i in range(mol_pyscf.nelec[1]):
             rdm1_MO[1][i, i] = 1
 
         # Check if the MO and AO basis RDM's are equal
-        assert np.allclose(mo_coeff_pyscf.dot(rdm1_MO[0].dot(mo_coeff_pyscf.T)),rdm1_HF[0]) 
-        assert np.allclose(mo_coeff_pyscf.dot(rdm1_MO[1].dot(mo_coeff_pyscf.T)),rdm1_HF[1]) 
-
-
-    # Get the HF energy from the mf module
-    e_HF = mf.e_tot
+        assert np.allclose(mo_coeff_pyscf.dot(rdm1_MO[0].dot(mo_coeff_pyscf.T)), rdm1_HF[0])
+        assert np.allclose(mo_coeff_pyscf.dot(rdm1_MO[1].dot(mo_coeff_pyscf.T)), rdm1_HF[1])
 
     # Transform the pyscf overlap and dipole integrals to the MO basis
-    overlap_MO = mo_coeff_pyscf.T.dot(overlap.dot(mo_coeff_pyscf))
+    overlap_MO = mo_coeff_pyscf.T.dot(overlap_pyscf.dot(mo_coeff_pyscf))
     dipole_MO = []
     for i in range(3):
-        dipole_MO.append(mo_coeff_pyscf.T.dot(dipole[i].dot(mo_coeff_pyscf))) 
-
-    # Store a molden checkpoint file in a temporary location to transfer the HF results to horton
-    tmpdir = tempfile.TemporaryDirectory()
-    checkpoint = tmpdir.name+f"temp.molden"
-    tools.molden.from_mo(mol_pyscf, checkpoint, mo_coeff_pyscf)
-    tmpdir.cleanup()
+        dipole_MO.append(mo_coeff_pyscf.T.dot(dipole[i].dot(mo_coeff_pyscf)))
 
     # Construct the molecule in Horton
-    mol_horton = IOData.from_file(checkpoint)
+    gobasis = from_pyscf(mol_pyscf)
 
-    # Create Gaussian Basis object
-    gb = GOBasis(centers=mol_horton.obasis['centers'], shell_map=mol_horton.obasis['shell_map'],nprims=mol_horton.obasis['nprims'],shell_types=mol_horton.obasis['shell_types'],alphas=mol_horton.obasis['alphas'],con_coeffs=mol_horton.obasis['con_coeffs'])
-    
     # Compute the overlap and dipole integrals from Horton, transform to MO basis
-    overlap_gb = gb.compute_overlap()
-    center = np.array([0.,0.,0.])
-    overlap_MO_gb = mol_horton.orb_alpha_coeffs.T.dot(overlap_gb).dot(mol_horton.orb_alpha_coeffs)
-    dipole_gb = np.array([gb.compute_multipole_moment(np.array([1, 0, 0]), center),gb.compute_multipole_moment(np.array([0, 1, 0]), center),gb.compute_multipole_moment(np.array([0, 0, 1]), center)])
+    overlap_gb = overlap.overlap_integral(gobasis)
+    center = np.array([0., 0., 0.])
+    overlap_MO_gb = mo_coeff_pyscf.T.dot(overlap_gb).dot(mo_coeff_pyscf)
+    dipole_array = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    dipole_gb = []
     dipole_MO_gb = []
     for i in range(3):
-        dipole_MO_gb.append(mol_horton.orb_alpha_coeffs.T.dot(dipole_gb[i].dot(mol_horton.orb_alpha_coeffs)))
+        dipole_gb.append(moment.moment_integral(gobasis, center, np.array([dipole_array[i]]))[:, :, 0])
+        dipole_MO_gb.append(mo_coeff_pyscf.T.dot(dipole_gb[i].dot(mo_coeff_pyscf)))
 
     # Check that the overlap and dipole integrals are the same in the MO basis coming from both pyscf and Horton
     assert np.allclose(overlap_MO, overlap_MO_gb)
     assert np.allclose(dipole_MO, dipole_MO_gb)
 
-    # Get the Horton MO coefficients. They are NOT identical to those from pyscf, since the AO's are ordered differently, but MO's should be ordered in the same way.
-    mo_coeff_horton = mol_horton.orb_alpha_coeffs
+    return mf, mo_coeff_pyscf, gobasis
 
-    return mf, mo_coeff_horton, gb
+
+def UHF(mol_pyscf):
+    '''Run a UHF calculation on mol_pyscf, does a few sanity checks between
+    horton and pyscf.
+
+    Arguments:
+    mol_pyscf: The molecule object from pyscf.
+
+    Returns:
+    mf: pyscf mean-field object.
+    mo_coeff_pyscf: pyscf MO coefficients.
+    gobasis: HORTON3 basiset object.'''
+
+    # Calculate the overlaps and dipoles using pyscf
+    overlap_pyscf = mol_pyscf.intor('int1e_ovlp')
+    dipole = mol_pyscf.intor('int1e_r')
+
+    # Run a RHF calculation on the molecule/atom
+    mf = scf.UHF(mol_pyscf).run()  #Run UHF
+
+    # Make HF 1-RDM in AO basis
+    rdm1_HF = mf.make_rdm1()
+
+    # Number of basis states
+    nbasis = rdm1_HF[0].shape[0]
+
+    # Get the MO coefficients C_{ai} from pyscf
+    mo_coeff_pyscf = mf.mo_coeff
+
+    # Make HF 1-RDM in MO basis
+    rdm1_MO = []
+    rdm1_MO.append(np.zeros((nbasis, nbasis)))
+    rdm1_MO.append(np.zeros((nbasis, nbasis)))
+    for i in range(mol_pyscf.nelec[0]):
+        rdm1_MO[0][i, i] = 1
+    for i in range(mol_pyscf.nelec[1]):
+        rdm1_MO[1][i, i] = 1
+
+    # Check if the MO and AO basis RDM's are equal
+    assert np.allclose(mo_coeff_pyscf[0].dot(rdm1_MO[0].dot(mo_coeff_pyscf[0].T)),rdm1_HF[0]) 
+    assert np.allclose(mo_coeff_pyscf[1].dot(rdm1_MO[1].dot(mo_coeff_pyscf[1].T)),rdm1_HF[1]) 
+
+    # Transform the pyscf overlap and dipole integrals to the MO basis
+    overlap_MO = [mo_coeff_pyscf[0].T.dot(overlap_pyscf.dot(mo_coeff_pyscf[0])),mo_coeff_pyscf[1].T.dot(overlap_pyscf.dot(mo_coeff_pyscf[1]))]
+    dipole_MO = []
+    for sigma in range(2):
+        dipole_MO.append([])
+        for i in range(3):
+            dipole_MO[sigma].append(mo_coeff_pyscf[sigma].T.dot(dipole[i].dot(mo_coeff_pyscf[sigma]))) 
+
+    # Construct the molecule in Horton
+    gobasis = from_pyscf(mol_pyscf)
+
+    # Compute the overlap and dipole integrals from Horton, transform to MO basis
+    overlap_MO_gb = []
+    overlap_gb = overlap.overlap_integral(gobasis)
+    center = np.array([0.,0.,0.])
+    dipole_list = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    dipole_gb = []
+    dipole_MO_gb = []
+
+    for sigma in range(2):
+        overlap_MO_gb.append(mo_coeff_pyscf[sigma].T.dot(overlap_gb).dot(mo_coeff_pyscf[sigma]))
+        dipole_MO_gb.append([])
+        for i in range(3):
+            dipole_gb.append(moment.moment_integral(gobasis, center, np.array([dipole_list[i]]))[:,:,0])
+            dipole_MO_gb[sigma].append(mo_coeff_pyscf[sigma].T.dot(dipole_gb[i].dot(mo_coeff_pyscf[sigma])))
+
+    # Check that the overlap and dipole integrals are the same in the MO basis coming from both pyscf and Horton
+    assert np.allclose(overlap_MO, overlap_MO_gb)
+    assert np.allclose(dipole_MO, dipole_MO_gb)
+
+    return mf, mo_coeff_pyscf, gobasis
 
 def fullCI(mol_pyscf, mf):
     ''' Run a fullCI calculation using pyscf on mol_pyscf
@@ -225,7 +291,7 @@ def MP2(mol_pyscf, mf):
     return e, rdm1, rdm2
 
 
-def compute_multipole_rdm12(nmax, rdm1, rdm2, mo_coeff_horton, gobasis, center=np.array([0.,0.,0.])):
+def compute_multipole_rdm12(nmax, rdm1, rdm2, mo_coeff, gobasis, center=np.array([0.,0.,0.])):
     '''Given a 1- and 2-rdm calculate the expectation values of the multipole moments up to power nmax given the mo_coefficients corresponding to horton, the 
     that is calculate <x^i y^j z^k> up to i + j + k = nmax and <x_1^i y_1^j z_1^k x_2^l y_2^m z_2^n> up to i+j+k=nmax/2 (even nmax) or i+j+k=(nmax+1)/2 (odd nmax) and same for l+m+n
 
@@ -242,43 +308,34 @@ def compute_multipole_rdm12(nmax, rdm1, rdm2, mo_coeff_horton, gobasis, center=n
     xyz2: expectation values of interaction multipoles.
     '''
 
-    #Extract the number of basis states from the 1-RDM
+    # Extract the number of basis states from the 1-RDM
     nbasis = rdm1.shape[0]
 
-    #See above, we don't need to calculate xyz2 up to nmax, but only halfnmax
+    # See above, we don't need to calculate xyz2 up to nmax, but only halfnmax
     halfnmax = np.ceil(nmax/2).astype(np.int)
 
-    #Build the empty matrix for the multipole moment in the AO and MO basis
+    # Build list of moments to compute.
+    momentlist = np.zeros((nmax**3, 3), dtype=np.int64)
+    n = 0
+    for i in range(nmax):
+        for j in range(nmax):
+            for k in range(nmax):
+                momentlist[n, :] = [i, j, k]
+                n += 1
 
-    xyzmat = np.zeros((nmax, nmax,nmax, nbasis,nbasis))
-    xyzmatMO = np.zeros((nmax, nmax,nmax, nbasis,nbasis))
-    xyz = np.zeros((nmax,nmax,nmax))
-    xyz2 = np.zeros((halfnmax,halfnmax,halfnmax,halfnmax,halfnmax,halfnmax))
-    
-    #Loop over the multipole powers, this should be sped up using numba
-    for i in range(0, nmax):
-        for j in range(0, nmax-i):
-            for k in range(0, nmax-i-j):
-                if i == j == k == 0:
-                    #compute_multipole_moment doesn't work for i = j = k = 0, call compute_overlap instead
-                    xyzmat[i, j, k, :, :] = gobasis.compute_overlap()
-                else:
-                    xyzmat[i, j, k, :, :] = gobasis.compute_multipole_moment(np.array([i, j, k]), center)
-                
-                #AO to MO transform
-                xyzmatMO[i,j,k,:,:] = np.einsum('ji,jk,kl->il',mo_coeff_horton,xyzmat[i,j,k,:,:],mo_coeff_horton,optimize=True)
-                
-                #Calculate expectation value from 1-RDM, do this in loop since we are looping anyway...
-                xyz[i,j,k] = np.einsum('ij,ij',rdm1,xyzmatMO[i,j,k,:,:],optimize=True)
+    #Compute multipoles with immediate AO to MO transform
+    xyzmatMO = moment.moment_integral(gobasis, center, momentlist, transform=mo_coeff.T).transpose(2, 0, 1).reshape(nmax, nmax, nmax, nbasis, nbasis)
 
+    #Calculate expectation value from 1-RDM, do this in loop since we are looping anyway...
+    xyz = np.einsum('ij,abcij->abc', rdm1, xyzmatMO, optimize=True)
 
     #One-liner for xyz2
-    xyz2 = np.einsum('ijkl,pqrij,stvkl->pqrstv',rdm2,xyzmatMO[0:halfnmax,0:halfnmax,0:halfnmax,:,:],xyzmatMO[0:halfnmax,0:halfnmax,0:halfnmax,:,:],optimize=True)
+    xyz2 = np.einsum('ijkl,pqrij,stvkl->pqrstv', rdm2, xyzmatMO[0:halfnmax, 0:halfnmax, 0:halfnmax, :, :], xyzmatMO[0:halfnmax, 0:halfnmax, 0:halfnmax, :, :], optimize=True)
     
     return xyz, xyz2
 
 
-def compute_multipole_RHF(Nel, nmax, mo_coeff_horton, gobasis, center=np.array([0., 0., 0.])):
+def compute_multipole_RHF(Nel, nmax, mo_coeff, gobasis, center=np.array([0., 0., 0.])):
     '''Given the number of electrons and the MO_coefficients for a RHF calculation 
     calculate the expectation values of the multipole moments up to power nmax given 
     the mo_coefficients corresponding to horton, that is calculate <x^i y^j z^k> 
@@ -297,46 +354,40 @@ def compute_multipole_RHF(Nel, nmax, mo_coeff_horton, gobasis, center=np.array([
     xyz2: expectation values of interaction multipoles. 
     CAVE: It's the expectation value with *minus* the exchange-hole.
     '''
-    #Build overlap matrix Smat
-    Smat = gobasis.compute_overlap()
+    # Build overlap matrix Smat
+    Smat = overlap.overlap_integral(gobasis)
 
-    #Extract the number of basis states from the overlap matrix
+    # Extract the number of basis states from the overlap matrix
     nbasis = Smat.shape[0]
 
-    #Number of occupied orbitals is half the number of electrons
+    # Number of occupied orbitals is half the number of electrons
     nocc = int(Nel//2)
 
-    #See above, we don't need to calculate xyz2 up to nmax, but only halfnmax
+    # See above, we don't need to calculate xyz2 up to nmax, but only halfnmax
     halfnmax = np.ceil(nmax/2).astype(np.int)
 
-    #Build the empty matrix for the multipole moment in the AO and MO basis
-    xyzmat = np.zeros((nmax, nmax,nmax, nbasis,nbasis))
-    xyzmatMO = np.zeros((nmax, nmax,nmax, nbasis,nbasis))
-    xyz = np.zeros((nmax,nmax,nmax))
-    xyz2 = np.zeros((halfnmax,halfnmax,halfnmax,halfnmax,halfnmax,halfnmax))
+    # Build list of moments to compute
+    momentlist = np.zeros((nmax**3, 3 ), dtype=np.int64)
+    n = 0
+    for i in range(nmax):
+        for j in range(nmax):
+            for k in range(nmax):
+                momentlist[n, :] = [i, j, k]
+                n += 1
 
-    #Loop over the multipole powers, this should be sped up using numba
-    for i in range(0,nmax):
-        for j in range(0,nmax-i):
-            for k in range(0,nmax-i-j):
-                if i == j == k == 0:
-                    #compute_multipole_moment doesn't work for i = j = k = 0, call compute_overlap instead
-                    xyzmat[i, j, k, :, :] = gobasis.compute_overlap()
-                else:
-                    xyzmat[i, j, k, :, :] = gobasis.compute_multipole_moment(np.array([i, j, k]), center)
-                
-                #AO to MO transform
-                xyzmatMO[i,j,k,:,:] = np.einsum('ji,jk,kl->il',mo_coeff_horton,xyzmat[i,j,k,:,:],mo_coeff_horton,optimize=True)
-                xyz[i,j,k] = 2*np.einsum('pp',xyzmatMO[i,j,k,:nocc,:nocc],optimize=True)
-    
+    # Compute multipoles with immediate AO to MO transform
+    xyzmatMO = moment.moment_integral(gobasis, center, momentlist, transform=mo_coeff.T).transpose(2, 0, 1).reshape(nmax, nmax, nmax, nbasis, nbasis)
+
+    xyz = 2*np.einsum('abcpp->abc', xyzmatMO[:, :, :, :nocc, :nocc], optimize=True)
+
     #One-liner for xyz2, coefficient is 1/2 (to get rid of opposite-spin exchange) * 2 (2 electrons per orbital) * 2 (2 electrons per orbital)
-    xyz2 = 2*np.einsum('ijkab,lmnab->ijklmn',xyzmatMO[0:halfnmax,0:halfnmax,0:halfnmax,:nocc,:nocc],xyzmatMO[0:halfnmax,0:halfnmax,0:halfnmax,:nocc,:nocc],optimize=True)
+    xyz2 = 2*np.einsum('ijkab,lmnab->ijklmn',xyzmatMO[0:halfnmax, 0:halfnmax, 0:halfnmax, :nocc, :nocc],xyzmatMO[0:halfnmax, 0:halfnmax, 0:halfnmax, :nocc, :nocc], optimize=True)
 
     #print("\n Computed xyz and xyz2 ")
     return xyz, xyz2
 
 
-def compute_multipole_ROHF(nelec, nmax, mo_coeff_horton, gobasis, center=np.array([0.,0.,0.])):
+def compute_multipole_ROHF(nelec, nmax, mo_coeff, gobasis, center=np.array([0.,0.,0.])):
     '''Given the number of electrons and the MO_coefficients for a ROHF calculation
     calculate the expectation values of the multipole moments up to power nmax given 
     the mo_coefficients corresponding to horton, that is calculate <x^i y^j z^k>
@@ -355,42 +406,37 @@ def compute_multipole_ROHF(nelec, nmax, mo_coeff_horton, gobasis, center=np.arra
     xyz2: expectation values of interaction multipoles. NOTE: It's the
     expectation value with *minus* the exchange-hole.
     '''
+    if type(gobasis) == 'tuple':
+        gobasis = gobasis[0]
 
-    #Build overlap matrix Smat
-    Smat = gobasis.compute_overlap()
+    # Build overlap matrix Smat
+    Smat = overlap.overlap_integral(gobasis)
 
-    #Extract number of basis states from overlap matrix
+    # Extract number of basis states from overlap matrix
     nbasis = Smat.shape[0]
 
-    #Extract separately number of alpha and beta electrons
+    # Extract separately number of alpha and beta electrons
     neleca, nelecb = nelec
 
-    #See above, we don't need to calculate xyz2 up to nmax, but only halfnmax
+    # See above, we don't need to calculate xyz2 up to nmax, but only halfnmax
     halfnmax = np.ceil(nmax/2).astype(np.int)
 
+    # Build list of moments to compute
+    momentlist = np.zeros((nmax**3, 3 ), dtype=np.int64)
+    n = 0
+    for i in range(nmax):
+        for j in range(nmax):
+            for k in range(nmax):
+                momentlist[n,:] = [i, j, k]
+                n += 1
 
-    #Build the empty matrix for the multipole moment in the AO and MO basis
-    xyzmat = np.zeros((nmax, nmax,nmax, nbasis,nbasis))
-    xyzmatMO = np.zeros((nmax, nmax,nmax, nbasis,nbasis))
-    xyz = np.zeros((nmax,nmax,nmax))
-    xyz2 = np.zeros((halfnmax,halfnmax,halfnmax,halfnmax,halfnmax,halfnmax))
+    # Compute multipoles with immediate AO to MO transform
+    xyzmatMO = moment.moment_integral(gobasis, center, momentlist, transform=mo_coeff.T).transpose(2, 0, 1).reshape(nmax, nmax, nmax, nbasis, nbasis)
+    xyz = np.einsum('abcpp->abc',xyzmatMO[:, :, :, :neleca, :neleca], optimize=True)+np.einsum('abcpp->abc', xyzmatMO[:, :, :, :nelecb, :nelecb],optimize=True)
 
-    #Loop over the multipole powers, this should be sped up using numba
-    for i in range(0,nmax):
-        for j in range(0,nmax-i):
-            for k in range(0,nmax-i-j):
-                if i == j == k == 0:
-                    #compute_multipole_moment doesn't work for i = j = k = 0, call compute_overlap instead
-                    xyzmat[i, j, k, :, :] = gobasis.compute_overlap()
-                else:
-                    xyzmat[i, j, k, :, :] = gobasis.compute_multipole_moment(np.array([i, j, k]), center)
-                
-                #AO to MO transform
-                xyzmatMO[i,j,k,:,:] = np.einsum('ji,jk,kl->il',mo_coeff_horton,xyzmat[i,j,k,:,:],mo_coeff_horton,optimize=True)
-                xyz[i,j,k] = np.einsum('pp',xyzmatMO[i,j,k,:neleca,:neleca],optimize=True)+np.einsum('pp',xyzmatMO[i,j,k,:nelecb,:nelecb],optimize=True)
-    #Two-liner for xyz2
-    xyz2 = np.einsum('ijkab,lmnab->ijklmn',xyzmatMO[0:halfnmax,0:halfnmax,0:halfnmax,:neleca,:neleca],xyzmatMO[0:halfnmax,0:halfnmax,0:halfnmax,:neleca,:neleca],optimize=True)
-    xyz2 += np.einsum('ijkab,lmnab->ijklmn',xyzmatMO[0:halfnmax,0:halfnmax,0:halfnmax,:nelecb,:nelecb],xyzmatMO[0:halfnmax,0:halfnmax,0:halfnmax,:nelecb,:nelecb],optimize=True)
+    # Two-liner for xyz2
+    xyz2 = np.einsum('ijkab,lmnab->ijklmn',xyzmatMO[0:halfnmax, 0:halfnmax, 0:halfnmax, :neleca, :neleca], xyzmatMO[0:halfnmax, 0:halfnmax, 0:halfnmax, :neleca, :neleca],optimize=True)
+    xyz2 += np.einsum('ijkab,lmnab->ijklmn',xyzmatMO[0:halfnmax, 0:halfnmax, 0:halfnmax, :nelecb, :nelecb],xyzmatMO[0:halfnmax, 0:halfnmax, 0:halfnmax, :nelecb, :nelecb],optimize=True)
 
-    #print("\n Computed xyz and xyz2 ")
+    # print("\n Computed xyz and xyz2 ")
     return xyz, xyz2
